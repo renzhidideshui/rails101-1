20545920b4eca4cf2a047d9fe4a680a2d2db1624
xing <13881308184@163.com>
2017-02-06 00:14:58 +0800

    implement groups#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,31 +6,17 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
-end
 
- def update
-   @group = Group.find(params[:id])
-
-   if @group.update(group_params)
-
-     redirect_to groups_path, notice: "Update Success"
-
-   end
- end
-
-
- def destroy
+  def create
      @group = Group.find(params[:id])
-     @group.destroy
-     flash[:alert] = "Group deleted"
-     redirect_to groups_path
-  end
+     @group.save
 
+        redirect_to groups_path
+end
 
  private
 
  def group_params
     params.require(:group).permit(:title, :description)
  end
-
 end
