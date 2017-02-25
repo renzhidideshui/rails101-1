009b31259229521fa91a2f62183e6f474ad6bd8b
xing <13881308184@163.com>
2017-02-25 12:44:38 +0800

    replace root with groups#index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
      flash[:notice] = "早安！ 张臭臭！起床床！"
-     flash[:alert] = "晚安！该睡了！"
-     flash[:warning] = "杨妈妈，这很棒！对吧"
-    end
+     flash[:alert]  = "晚安！ 该睡了"
+     flash[:warning]= "杨妈妈，这很棒！对吧！"
+  end
 end
