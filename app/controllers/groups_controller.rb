95362383c4a8d119410d52a15044527a691268c9
xing <13881308184@163.com>
2017-02-25 22:15:28 +0800

    style form with bootstrap

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,22 +3,50 @@ class GroupsController < ApplicationController
     @groups =Group.all
   end
 
-def show
-  @group = Group.find(params[:id])
-end
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
 
   def new
     @group = Group.new
   end
-end
 
-def create
-  @group = Group.new(group_params)
-  @group.save
 
-     redirect_to groups_path
-end
+  def create
+    @group = Group.new(group_params)
+    if @group.save
 
-def group_params
-  params.require(:group).permit(:title, :desciption)
+    redirect_to groups_path
+  else
+    render :new
+   end
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+     if @group.update(group_params)
+
+    redirect_to groups_path,notice: "Update Success"
+  else
+      render :edit
+    end
+  end
+
+  def  destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to groups_psth
+  end
+
+  private
+
+  def group_params
+     params.require(:group).permit(:title, :description)
+  end
 end
